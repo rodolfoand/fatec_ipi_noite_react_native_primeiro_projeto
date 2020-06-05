0e8e2f13296c774c659936271d311af2bdde352f
rodolfoand <rodolfo.andrade@fatec.sp.gov.br>
2020-06-05 19:58:20 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [texto, setTexto] = useState('Texto Inicial');
+  const [contador, setContador] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{marginBottom: 16 }}>{contador}</Text>
+      <Button
+       title="Alterar texto" 
+       onPress={() => setContador(contador + 1)} />
     </View>
   );
 }
